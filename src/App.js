f9d203b37bfd26f65cdcd29e1117da6e07d60d0a
Mizraelko <vkoporov@gmail.com>
2021-01-17 22:08:01 +0300

    job

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import './App.css';
 import React from "react";
-
 function App() {
-  return (
+
+
+    const switchTheme = (bool) => () => {
+
+        const arr = ['https://stackpath.bootstrapcdn.com/bootswatch/4.5.2/sketchy/bootstrap.min.css'];
+        const ref = document.querySelector('head').querySelector('Link');
+        if(bool) {
+            ref.href = arr[0]
+        }
+        console.log(ref)
+    }
+
+
+
+
+
+    // const switchStyleHead = () => {
+    //
+    //
+    //
+    //
+    //     return documentq
+        //document.querySelector('head').innerHTML += '<link rel="stylesheet" href="styles.css" type="text/css"/>';
+
+    return (
     <div className="App">
-
-
+        <button onClick={() => {switchTheme(true)}}>
+            switch
+        </button>
     </div>
-  )
+    )
 }
 
 export default App;
---
+++ src/stylesBootstrap.js
@@ -0,0 +1,9 @@
+const switchStyleHead = () => {
+
+    let documentq =  document.querySelector('head').querySelector('Link')
+
+    document.querySelector('head').getAttribute('href')
+    console.log(documentq)
+}
+
+switchStyleHead()
